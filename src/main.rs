b9ae0cc6f37f64267beb36a2e0281f2aa1880c47
Felix Dallmeyer <felix.dallmeyer1@gmail.com>
2024-12-12 14:07:55 +0100

    pretty output Taschenrechner entfernt prettytables hinzugefügt

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,32 @@
 use clap::Parser;
+#[macro_use] extern crate prettytable;
+use prettytable::{Table,format};
+
 
 /// Simple program to greet a person
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    
-    o1: f64,
-    
-    o2: f64,
+    #[arg(long)]
+    haltestelle: String,
 
-    operation: String,
+    #[arg(short,long)]
+    id: f32,
 
+    #[arg(short,long)]
+    arrival: String,
+
+    #[arg(short,long)]
+    departure: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let result = match args.operation.as_str() {
-        "add" => args.o1 + args.o2,
-        "sub" => args.o1 - args.o2,
-        "mul" => args.o1 * args.o2,
-        "div" => {
-            if args.o2 == 0.0 {
-                println!("Tuhe das nicht!!!!!!!");
-                return;
-            }
-            args.o1 / args.o2
-        }
-        _ => {
-            println!("Invalid operation! Use +, -, *, or /");
-            return;
-        }
-    };
-
-    println!("{} {} {} = {}", args.o1, args.operation, args.o2, result);
-}
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_BOX_CHARS);
+    table.add_row(row!["Haltestelle","Bus/Bahn ID","Ankfunft","Abfahrt"]);
+    table.add_row(row![args.haltestelle ,args.id ,args.arrival,args.departure]);
+    table.printstd();
+    }
