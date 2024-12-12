dddc11049b7ec7f0ea730db73ff21361f05335d3
Felix Dallmeyer <felix.dallmeyer1@gmail.com>
2024-12-12 22:47:25 +0100

    erste api request

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::Parser;
-#[macro_use] extern crate prettytable;
-use prettytable::{Table,format};
-
+use reqwest::Error;
+use dotenv::dotenv;
+use std::env;
+#[macro_use]
+extern crate prettytable;
+use prettytable::{format, Table};
 
 /// Simple program to greet a person
 
@@ -11,22 +14,47 @@ struct Args {
     #[arg(long)]
     haltestelle: String,
 
-    #[arg(short,long)]
-    id: f32,
+    #[arg(short, long)]
+    id: i32,
 
-    #[arg(short,long)]
+    #[arg(short, long)]
     arrival: String,
 
-    #[arg(short,long)]
+    #[arg(short, long)]
     departure: String,
 }
 
-fn main() {
+ async fn get_request(url:&str) -> Result<(),Error>{
+let response = reqwest::get(url).await?;
+    println!("Status: {}", response.status());
+
+    let body = response.text().await?;
+    println!("Body:\n{}", body);
+
+    Ok(())
+ }
+ #[tokio::main]
+ async fn main() -> Result<(),Error> {
     let args = Args::parse();
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("env-var API_KEY muss gesetzt sein!\n");
+    let url = format!("https://www.rmv.de/hapi/location.name?accessId={}&input=frankfurt%20hauptbahnhof&format=json",api_key);
+    get_request(&url).await?;
+    // let client = reqwest::Client::new();
+    // let data = client.get("https://httpbin.org/get")
+    //     .await?
+    //     .text()
+    //     .await?;
 
     let mut table = Table::new();
     table.set_format(*format::consts::FORMAT_BOX_CHARS);
-    table.add_row(row!["Haltestelle","Bus/Bahn ID","Ankfunft","Abfahrt"]);
-    table.add_row(row![args.haltestelle ,args.id ,args.arrival,args.departure]);
+    table.add_row(row!["Haltestelle", "Bus/Bahn ID", "Ankfunft", "Abfahrt"]);
+    table.add_row(row![
+        args.haltestelle,
+        args.id,
+        args.arrival,
+        args.departure
+    ]);
     table.printstd();
-    }
+    Ok(())
+}
